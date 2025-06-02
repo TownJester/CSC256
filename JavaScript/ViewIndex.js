83c20d9096602195006b5c94349a96f14e3c6100
Madalyn <madalynshea@icloud.com>
2025-06-02 12:47:17 -0700

    Add files via upload

---
+++ JavaScript/ViewIndex.js
@@ -0,0 +1,19 @@
+document.getElementById("Heading").textContent = "Index";
+document.getElementById("Introduction").textContent = "Hello, my name is Madalyn Cook and this is a website to revist the basics. Below contains information about me and this assignment. ";
+document.getElementById("DSubH").textContent = "Assignment Details";
+document.getElementById("ADetails").textContent = "Create a very basic INDEX.HTML page and a very basic MAIN.CSS that shows your name, major, UAT email address, and expected graduation date (May 2025 for example). You do not need to include a photo with this assignment. This is to refresh your memory of HTML/CSS and we will build upon this webpage during this class.";
+document.getElementById("ISubH").textContent = "Information About Me";
+document.getElementById("Name").textContent = "Name: Madalyn Cook";
+document.getElementById("Age").textContent = "Age: 19 as of 2025";
+document.getElementById("School").textContent = "School: University of Advancing Technology";
+document.getElementById("Major").textContent = "Major: Advancing Computer Science";
+document.getElementById("Graduation").textContent = "Graduation Date: Fall, 2027";
+document.getElementById("Contacts").textContent = "Ways to Contact Me!";
+document.getElementById("SchoolEmail").textContent = "School Email: MCOOK87275@uat.edu";
+document.getElementById("PersonalEmail").textContent = "Personal Email: S.Madalny.C@gmail.com";
+
+
+/*
+ 5/28/2025
+6/2/2025
+*/
